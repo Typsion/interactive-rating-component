e6d69f866ce3f00614b50ba71eba0ce62e200bf9
Typsion <majajegorow.dev@gmail.com>
2026-05-01 22:17:56 +0200

    Update script.js
    Improve script

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let selectedRating  = 0;
     
-function pokazKarte() {
+function showCard() {
     if(selectedRating === 0){
       alert("Please select a rating before submitting!");
-      document.getElementById(".btn").style.backgroundColor = "hsl(25, 97%, 53%)";
+      document.getElementById("btn").style.backgroundColor = "hsl(25, 97%, 53%)";
       return;
     }
-    document.querySelector(".card:not(.card--new)").style.display = "none";
-    document.getElementById("nowaKarta").style.display = "block";
+    document.getElementById("ratingCard").style.display = "none";
+    document.getElementById("thankyouCard").style.display = "block";
     document.getElementById("ratingValue").textContent = selectedRating;
 }
     
 function clickedPoint(value, element) {
     selectedRating = value;
     const points = document.querySelectorAll(".point");
-    points.forEach(el => el.classList.remove("active"));
+
+    points.forEach(el => {
+        el.classList.remove("active");
+        el.setAttribute("aria-checked", "false");
+    });
     element.classList.add("active");
+    element.setAttribute("aria-checked", "true");
 }
